e23365db661eedc5984a4f98e581cdcd18ee5e21
flyingb5000 <flyingb5000@hotmail.com>
2025-03-23 22:25:50 +0800

    change the bat file

--- run_sell_put_screener.bat
+++ run_sell_put_screener.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Starting Options Screener...
 cd /d "d:\Lenovo Ideapad D drive\VS code projects\options-trading"
-python options_screener_ui.py
+python sell_put_screener_ui.py
 pause
